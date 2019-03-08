e73c2f2279de52a695d2093334c506998c420f4a
Marcin Karski <karski80@gmail.com>
2019-03-08 14:20:22 +0000

    Initial TabBarController setup

--- TabBarController/ViewController.swift
+++ TabBarController/ViewController.swift
@@ -4,6 +4,28 @@ class ViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        
+        let todayViewController = UIViewController()
+        todayViewController.view.backgroundColor = .red
+        todayViewController.navigationItem.title = "Today"
+        let todayNavController = UINavigationController(rootViewController: todayViewController)
+        todayNavController.tabBarItem.title = "Today"
+        todayNavController.navigationBar.prefersLargeTitles = true
+        
+        let appsViewController = UIViewController()
+        appsViewController.view.backgroundColor = .blue
+        appsViewController.navigationItem.title = "Apps"
+        let appsNavController = UINavigationController(rootViewController: appsViewController)
+        appsNavController.tabBarItem.title = "Apps"
+        appsNavController.navigationBar.prefersLargeTitles = true
+        
+        let searchViewController = UIViewController()
+        searchViewController.view.backgroundColor = .green
+        searchViewController.navigationItem.title = "Search"
+        let searchNavController = UINavigationController(rootViewController: searchViewController)
+        searchNavController.tabBarItem.title = "Search"
+        searchNavController.navigationBar.prefersLargeTitles = true
+        
+        viewControllers = [todayNavController, appsNavController, searchNavController]
     }
 }
